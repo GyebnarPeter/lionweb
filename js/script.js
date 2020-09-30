caa72b6448ae16967d4ce7cf98bb38a58a1fca58
unknown <gyebnarpetter@gmail.com>
2020-09-30 16:27:19 +0200

    services part

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 // Cursor effect
+const cursor = document.querySelector('.cursor');
 (function() {
     
     // Nav animation
     const link = document.querySelectorAll('nav > .hover-this');
-    const cursor = document.querySelector('.cursor');
     
     const animateit = function (e) {
         const span = this.querySelector('span');
@@ -38,16 +38,17 @@ let headerLogo = document.querySelector(".header-logo"),
     navBtnScroll = document.querySelector(".nav-btn-scroll"),
     navCloseBtn = document.querySelector(".nav-close-btn"),
     webshopBtn = document.querySelector(".webshop-btn"),
-    coverTitle = document.querySelectorAll(".cover h1 span");
+    coverTitle = document.querySelectorAll(".cover h1 span"),
     allLink = document.querySelectorAll("a"),
+    servicesImg = document.querySelectorAll(".services-picture img"),
     hoverItems = [headerLogo, navBtnFix, webshopBtn, navBtnScroll, navCloseBtn];
 
 let cursorHover = (item) => {
     item.addEventListener("mouseover", () => {
-        document.querySelector(".cursor").classList.add("cursor-hover");
+        cursor.classList.add("cursor-hover");
     })
     item.addEventListener("mouseout", () => {
-        document.querySelector(".cursor").classList.remove("cursor-hover");
+        cursor.classList.remove("cursor-hover");
     })
 };
 
@@ -59,12 +60,16 @@ allLink.forEach( (item) => {
     cursorHover(item);
 });
 
+servicesImg.forEach( (item) => {
+    cursorHover(item);
+});
+
 coverTitle.forEach( (item) => {
     item.addEventListener("mouseover", () => {
-        document.querySelector(".cursor").classList.add("cursor-hover-cover");
+        cursor.classList.add("cursor-hover-cover");
     })
     item.addEventListener("mouseout", () => {
-        document.querySelector(".cursor").classList.remove("cursor-hover-cover");
+        cursor.classList.remove("cursor-hover-cover");
     })
 });
 
@@ -84,3 +89,44 @@ navcloseBtn.onclick = () => {
     nav.classList.remove("nav-wrap-toggle");
     navBtnFix.style.display = "flex";
 }
+
+
+// Services image scroll cover element
+window.onscroll = () => {
+    let servicesCoverItem = document.querySelector(".services-cover-item");
+    let services = document.querySelector(".services-wrap");
+    let servicesPositionTop = services.offsetTop;
+    let servicesPositionBottom = servicesPositionTop + services.offsetHeight;
+    if (document.documentElement.scrollTop > servicesPositionTop && document.documentElement.scrollTop < servicesPositionBottom - window.innerHeight) {
+        servicesCoverItem.style.display = "block";
+    } else {
+        servicesCoverItem.style.display = "none";
+    }
+}
+
+
+
+
+/* (function() {
+    
+    // Nav animation
+    const link = document.querySelectorAll('.services-picture');
+    
+    const animateit = function (e) {
+        const figure = this.querySelector('figure');
+        const { offsetX: x, offsetY: y } = e,
+        { offsetWidth: width, offsetHeight: height } = this,
+
+              move = 25,
+              xMove = x / width * (move * 2) - move,
+              yMove = y / height * (move * 2) - move;
+
+        figure.style.transform = `translate(${xMove}px, ${yMove}px)`;
+        
+        if (e.type === 'mouseleave') figure.style.transform = '';
+    };
+    
+    link.forEach(b => b.addEventListener('mousemove', animateit));
+    link.forEach(b => b.addEventListener('mouseleave', animateit));
+    
+})(); */
